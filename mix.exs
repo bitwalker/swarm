bc5630930848d549868b2a60a63aee445d829854
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-08-04 01:06:11 -0500

    Version 0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Swarm.Mixfile do
 
   def project do
     [app: :swarm,
-     version: "0.3.1",
+     version: "0.4.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

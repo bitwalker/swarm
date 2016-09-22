d90747314dacfd4d30eba7e87cd8eb01da0927e3
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-09-22 16:28:56 -0500

    Remove node monitoring from EPMD strategy, no longer needed

--- lib/swarm/cluster/epmd.ex
+++ lib/swarm/cluster/epmd.ex
@@ -1,29 +1,6 @@
 defmodule Swarm.Cluster.Epmd do
   @moduledoc """
-  This clustering strategy is effectively a no-op
+  This clustering strategy relies on Erlang's built-in distribution protocol
   """
-  use GenServer
-  import Swarm.Logger
-
   def start_link(), do: :ignore
-  def init(_) do
-    :ok = :net_kernel.monitor_nodes(true, [node_type: :all])
-    {:ok, []}
-  end
-
-  def handle_info({:nodeup, node, _info}, state) do
-    handle_info({:nodeup, node}, state)
-  end
-  def handle_info({:nodeup, node}, state) do
-    debug "nodeup #{node}"
-    {:noreply, state}
-  end
-  def handle_info({:nodedown, node, _info}, state) do
-    handle_info({:nodedown, node}, state)
-  end
-  def handle_info({:nodedown, node}, state) do
-    debug "nodedown #{node}"
-    {:noreply, state}
-  end
-  def handle_info(_, state), do: {:noreply, state}
 end

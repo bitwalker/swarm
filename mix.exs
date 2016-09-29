55979e16bebca63d6bc4c9576a3be6b201fc3bd5
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-09-29 15:23:28 -0500

    Version 1.1.2

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Swarm.Mixfile do
 
   def project do
     [app: :swarm,
-     version: "1.1.1",
+     version: "1.1.2",
      elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,

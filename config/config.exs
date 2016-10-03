a89d49b6891bd96b09b89a7baf5c0509bdee67ed
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-10-03 09:52:53 -0500

    Update config example

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,18 @@ use Mix.Config
 config :swarm,
   debug: false,   # turn on debugging mode
   debug_opts: [], # [:trace] for detailed logging
-  node_blacklist: [~r/^remsh.*$/],
-  node_whitelist: []
+  node_blacklist: [
+    # the following blacklists nodes set up by exrm/relx/distillery
+    # for remote shells (the first) and hot upgrade scripting (the second)
+    ~r/^remsh.*$/,
+    ~r/.+_upgrader_.+/
+    # or using strings..
+    # "some_node" - literals
+    # "^remsh.*$" - regex patterns
+  ],
+  node_whitelist: [
+    # the same type of list as node_blacklist
+  ]
 
 config :logger,
   level: :info

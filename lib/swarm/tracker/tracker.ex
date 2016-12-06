b73cd9a8005b0614e0028015b559c59a8171bfa7
Paul Schoenfelder <paulschoenfelder@gmail.com>
2016-12-05 18:19:21 -0600

    Add maint nodes to default blacklist

--- lib/swarm/tracker/tracker.ex
+++ lib/swarm/tracker/tracker.ex
@@ -1222,7 +1222,7 @@ defmodule Swarm.Tracker do
     end
   end
 
-  @default_blacklist [~r/^remsh.*$/, ~r/^.+_upgrader_.+$/]
+  @default_blacklist [~r/^remsh.*$/, ~r/^.+_upgrader_.+$/, ~r/^.+_maint_.+$/]
   # The list of configured ignore patterns for nodes
   # This is only applied if no whitelist is provided.
   defp node_blacklist(), do: Application.get_env(:swarm, :node_blacklist, @default_blacklist)

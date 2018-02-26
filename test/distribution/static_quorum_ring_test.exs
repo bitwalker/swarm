da4b80f738b5c167ada6f860a083823b054b08a7
Bartosz Kalinowski <kelostrada@gmail.com>
2018-02-26 14:10:40 +0100

    added test for static quorum ring size

--- test/distribution/static_quorum_ring_test.exs
+++ test/distribution/static_quorum_ring_test.exs
@@ -18,4 +18,13 @@ defmodule Swarm.Distribution.StaticQuorumRingTests do
     quorum = StaticQuorumRing.add_node(quorum, "node3")
     assert StaticQuorumRing.key_to_node(quorum, :key1) != :undefined
   end
+
+  test "quorum size should be set by binary setting" do
+    static_quorum_size = Application.get_env(:swarm, :static_quorum_size)
+    Application.put_env(:swarm, :static_quorum_size, "5")
+
+    assert StaticQuorumRing.create() == %StaticQuorumRing{ring: %HashRing{}, static_quorum_size: 5}
+
+    Application.put_env(:swarm, :static_quorum_size, static_quorum_size)
+  end
 end

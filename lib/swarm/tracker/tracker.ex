711cd4a4dfcae37984220f60865859a3834eb950
Jesse <jesse@gigalixir.com>
2019-05-14 15:49:23 -0700

    Revert "Remove unused casts. All tests still pass. Not sure if they are unused or if they should be calls instead of casts."
    This reverts commit a149655693fc30e7ddd97cf91afb0a79df241ecf.

--- lib/swarm/tracker/tracker.ex
+++ lib/swarm/tracker/tracker.ex
@@ -1593,7 +1593,9 @@ defmodule Swarm.Tracker do
          entry(name: rname, pid: rpid),
          state
        ) do
+    GenStateMachine.cast({__MODULE__, remote_node}, {:untrack, rpid})
     send(rpid, {:swarm, :die})
+    GenStateMachine.cast({__MODULE__, remote_node}, {:track, rname, lpid, lmeta})
     state
   end
 
